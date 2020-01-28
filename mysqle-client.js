f36f1e66cf4a042294e8599f3e7b6d3b78f72af3
jaslocum <jack@surfacefinishtech.com>
2020-01-28 11:45:42 -0500

    latest mysqle development version

--- mysqle-client.js
+++ mysqle-client.js
@@ -1,41 +1,47 @@
 const io = require('socket.io-client')
 
-// module private var
-let socket = null
-let mysqleEvents = {}
-let mysqleUri = null
-let mysqleId = null
-let mysqleStatus = {}
-let mysqleConnected = 0
+class mysqle {
 
-let mysqle = {
-
-  on: function (event, callback) {
-    eventAdd(event)
-    if (socketIsset()) {
-      socket.on(event, callback)
-      eventsEmit()
+  constructor(connect) {
+    this.socket = null
+    this.mysqleEvents = {}
+    this.mysqleUri = null
+    this.mysqleId = null
+    this.mysqleStatus = {}
+    this.mysqleConnected = 0
+    if (this.connect(connect)) {
       return true
     } else {
       return false
     }
-  },
+  }
 
-  off: function (event) {
-    if (eventIsset(event)) {
-      socket.off(event)
-      eventRemove(event)
-      eventsEmit()
+  on(event, callback) {
+    this.eventAdd(event)
+    if (this.socketIsSet()) {
+      this.socket.on(event, callback)
+      this.eventsEmit()
       return true
     } else {
       return false
     }
-  },
+  }
 
-  connect: function (config) {
-    if (this.config(config)) {
-      if (socketOn()) {
-        listen()
+  off(event) {
+    if (this.eventIsSet(event)) {
+      this.socket.off(event)
+      this.eventRemove(event)
+      this.eventsEmit()
+      return true
+    } else {
+      return false
+    }
+  }
+
+  connect(connect) {
+    if (this.config(connect)) {
+      if (this.socketOn()) {
+        this.listen()
         return true
       } else {
         return false
@@ -43,177 +49,55 @@ let mysqle = {
     } else {
       return false
     }
-  },
+  }
 
-  disconnect: function () {
-    if (socketConnected()) {
-      socketOff()
-      eventsClear()
+  disconnect() {
+    if (this.socketConnected()) {
+      this.socketOff()
+      this.eventsClear()
       return true
     } else {
       return false
     }
-  },
+  }
 
-  config: function (config) {
-    uriSet(config)
-    return configGet()
-  },
+  config(config) {
+    this.uriSet(config)
+    return this.configGet()
+  }
 
-  uri: function (uri) {
-    uriSet(uri)
-    return uriGet()
-  },
+  uri(uri) {
+    this.uriSet(uri)
+    return this.uriGet()
+  }
 
-  id: function () {
-    return idGet()
-  },
+  id() {
+    return this.idGet()
+  }
 
-  events_array: function () {
-    if (eventsIsset()) {
-      return Object.keys(eventsGet())
-    } else {
-      return null
-    }
-  },
-
-  events_string: function () {
-    return JSON.stringify(eventsGet())
-  },
-
-  status: function () {
-    if (statusEmit()) {
-      return mysqleStatus
+  eventsArray() {
+    if (this.eventsIsSet()) {
+      return Object.keys(this.eventsGet())
     } else {
       return null
     }
   }
-}
 
-function socketConnected () {
-  if (socketIsset()) {
-    if (idIsset()) {
-      return true
+  eventsString() {
+    return JSON.stringify(this.eventsGet())
+  }
+
+  status() {
+    if (this.statusEmit()) {
+      return this.mysqleStatus
     } else {
-      return false
+      return null
     }
-  } else {
-    return false
   }
-}
 
-function socketIsset () {
-  if (socket != null) {
-    return true
-  } else {
-    return false
-  }
-}
-
-function socketOn () {
-  if (!socketConnected()) {
-    socket = io(uriGet(), {
-      cookie: false
-    })
-    return true
-  } else {
-    return false
-  }
-}
-
-function socketOff () {
-  if (socketConnected()) {
-    socket.disconnect()
-    socket = null
-    return true
-  } else {
-    return false
-  }
-}
-
-function connectedInc () {
-  mysqleConnected++
-  statusSet('connected', mysqleConnected)
-  return true
-}
-
-function socketsSet (sockets) {
-  if (typeof sockets !== 'undefined') {
-    statusSet('sockets', sockets)
-    return true
-  } else {
-    return false
-  }
-}
-
-function uriGet () {
-  if (mysqleUri != null) {
-    return mysqleUri
-  } else {
-    return null
-  }
-}
-
-function uriSet (uri) {
-  if (typeof uri !== 'undefined' &&
-    typeof uri === 'string') {
-    mysqleUri = uri
-    statusSet('uri', uri)
-    return true
-  } else {
-    return false
-  }
-}
-
-function configGet () {
-  var config = {
-    uri: uriGet(),
-    events: mysqle.events_array()
-  }
-  return config
-}
-
-function idGet () {
-  if (idIsset()) {
-    return mysqleId
-  } else {
-    return null
-  }
-}
-
-function idIsset () {
-  if (typeof mysqleId === 'string' &&
-    mysqleId.length > 0) {
-    return true
-  } else {
-    return false
-  }
-}
-
-function eventAdd (event) {
-  if (event != null) {
-    mysqleEvents[event] = true
-    statusSet('events', mysqle.events_string())
-    return true
-  } else {
-    return false
-  }
-}
-
-function eventRemove (event) {
-  if (event != null) {
-    delete mysqleEvents[event]
-    statusSet('events', mysqle.events_string())
-    return true
-  } else {
-    return false
-  }
-}
-
-function eventIsset (event) {
-  if (typeof event === 'string') {
-    if (eventsIsset()) {
-      if (event in eventsGet()) {
+  socketConnected () {
+    if (this.socketIsSet()) {
+      if (this.idIsSet()) {
         return true
       } else {
         return false
@@ -221,86 +105,207 @@ function eventIsset (event) {
     } else {
       return false
     }
-  } else {
-    return false
   }
-}
 
-function eventsClear () {
-  mysqleEvents = {}
-  statusSet('events', mysqle.events_string())
-  return true
-}
-
-function eventsGet () {
-  if (eventsIsset()) {
-    return mysqleEvents
-  } else {
-    return null
+  socketIsSet () {
+    if (this.socket != null) {
+      return true
+    } else {
+      return false
+    }
   }
-}
 
-function eventsIsset () {
-  if (mysqleEvents != null &&
-    Object.keys(mysqleEvents).length > 0) {
-    return true
-  } else {
-    return false
-  }
-}
-
-function eventsEmit () {
-  socket.emit('events', mysqle.events_array())
-}
-
-function setId (id) {
-  if (id != null) {
-    mysqleId = id
-    statusSet('id', id)
-    return true
-  } else {
-    return false
-  }
-}
-
-function statusSet (key, value) {
-  if (key != null &&
-    typeof value !== 'undefined') {
-    mysqleStatus[key] = value
-    return true
-  } else {
-    return false
-  }
-}
-
-function statusEmit () {
-  if (typeof mysqleStatus !== 'undefined' &&
-    Object.keys(mysqleStatus).length > 0) {
-    socket.emit('status', mysqleStatus)
-    return true
-  } else {
-    return false
-  }
-}
-
-function listen () {
-  if (socketIsset()) {
-    socket.on('connect', function () {
-      var currentdate = new Date().toLocaleString()
-      statusSet('connected_at', currentdate)
-      connectedInc()
-      setId(socket.id)
-      socket.on('sockets', function (sockets) {
-        socketsSet(sockets)
-        statusEmit()
+  async socketOn () {
+    if (!this.socketConnected()) {
+      this.socket = await io(this.uriGet(), {
+        cookie: false
       })
-    })
-    return true
-  } else {
-    return false
+      return true
+    } else {
+      return false
+    }
   }
+
+  socketOff () {
+    if (this.socketConnected()) {
+      this.socket.disconnect()
+      this.socket = null
+      return true
+    } else {
+      return false
+    }
+  }
+
+  connectedInc () {
+    this.mysqleConnected++
+    this.statusSet('connected', this.mysqleConnected)
+    return true
+  }
+
+  socketsSet (sockets) {
+    if (typeof sockets !== 'undefined') {
+      this.statusSet('sockets', sockets)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  uriGet () {
+    if (this.mysqleUri != null) {
+      return this.mysqleUri
+    } else {
+      return null
+    }
+  }
+
+  uriSet (uri) {
+    if (typeof uri !== 'undefined' &&
+      typeof uri === 'string') {
+      this.mysqleUri = uri
+      this.statusSet('uri', uri)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  configGet () {
+    let config = {
+      uri: this.uriGet(),
+      events: this.eventsArray()
+    }
+    return config
+  }
+
+  idGet () {
+    if (this.idIsSet()) {
+      return this.mysqleId
+    } else {
+      return null
+    }
+  }
+
+  idIsSet () {
+    if (typeof this.mysqleId === 'string' &&
+      this.mysqleId.length > 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  eventAdd (event) {
+    if (event != null) {
+      this.mysqleEvents[event] = true
+      this.statusSet('events', this.eventsString())
+      return true
+    } else {
+      return false
+    }
+  }
+
+  eventRemove (event) {
+    if (event != null) {
+      delete this.mysqleEvents[event]
+      this.statusSet('events', this.eventsString())
+      return true
+    } else {
+      return false
+    }
+  }
+
+  eventIsSet (event) {
+    if (typeof event === 'string') {
+      if (this.eventsIsSet()) {
+        if (event in this.eventsGet()) {
+          return true
+        } else {
+          return false
+        }
+      } else {
+        return false
+      }
+    } else {
+      return false
+    }
+  }
+
+  eventsClear () {
+    this.mysqleEvents = {}
+    this.statusSet('events', this.eventsString())
+    return true
+  }
+
+  eventsGet () {
+    if (this.eventsIsSet()) {
+      return this.mysqleEvents
+    } else {
+      return null
+    }
+  }
+
+  eventsIsSet () {
+    if (this.mysqleEvents != null &&
+      Object.keys(this.mysqleEvents).length > 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  eventsEmit () {
+    this.socket.emit('events', this.eventsArray())
+  }
+
+  setId (id) {
+    if (id != null) {
+      this.mysqleId = id
+      this.statusSet('id', id)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  statusSet (key, value) {
+    if (key != null &&
+      typeof value !== 'undefined') {
+      this.mysqleStatus[key] = value
+      return true
+    } else {
+      return false
+    }
+  }
+
+  statusEmit () {
+    if (typeof this.mysqleStatus !== 'undefined' &&
+      Object.keys(this.mysqleStatus).length > 0) {
+      this.socket.emit('status', this.mysqleStatus)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  listen () {
+    if (this.socketIsSet()) {
+      this.socket.on('connect', () => {
+        let currentdate = new Date().toLocaleString()
+        this.statusSet('connected_at', currentdate)
+        this.connectedInc()
+        this.setId(this.socket.id)
+        this.socket.on('sockets', sockets => {
+          this.socketsSet(sockets)
+          this.statusEmit()
+        })
+      })
+      return true
+    } else {
+      return false
+    }
+  }
+
 }
-/**
- * Module exports.
- */
+
 module.exports = mysqle
