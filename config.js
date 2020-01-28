33ac85b5d6de635cf676681744cae7460a5c8a76
jaslocum <jack@surfacefinishtech.com>
2020-01-28 08:04:01 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,36 @@
+let config = {
+
+  /*
+    database connection
+  */
+  connection: {
+    host: 'eagle',
+    port: '3306',
+    user: 'repl',
+    password: 'secret',
+    timezone: 480
+  },
+
+  /*
+    must be specified and unique for each instance of ZongJi reading binlog events for database
+  */
+  serverId: 12,
+  
+  /*
+    port for event clients to connect to
+  */
+  port: '3308',
+
+  /*
+    connection retry settings
+  */
+  connectMax: 30,
+
+  /*
+    logging status
+  */
+  logging: true
+
+}
+
+module.exports = config
